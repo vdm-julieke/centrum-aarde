262d4ef135e0401c1c7465dd43e10db929b552c1
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-03-07 12:57:58 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showArrow(ArrowNames.South)
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 })
